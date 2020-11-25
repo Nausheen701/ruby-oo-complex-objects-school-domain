39ad40b7ee5d9b14ee79d7df406cde38ffbbc968
Nausheen Akhter <nausheen701@gmail.com>
2020-11-26 01:15:32 +0300

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+
+    def initialize(name)
+        @name = name
+        #A school should have a roster. The roster should be an empty hash upon initialization but will be built out to contain keys of grade levels. The value of each key will be an array of student names.
+        roster = {} 
+    end
+    def name=(name)
+        @name = name
+    end
+
+    def name
+        @name
+    end 
+    
+    def add_student(student_name, student_grade)
+        @student_name = student_name
+        @student_grade = student_grade
+    end
+
+
+
+        #roster << add_student
+    end
+
+
+end
